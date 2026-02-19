a02846f593f79311ac274185dd2011b9aea9e9b3
first <first@windows.windows>
2026-02-19 16:39:50 -0300

    Made the code output the decoded strings to a text file and could skim it to find the encrypted line with XOR

--- main.c
+++ main.c
@@ -1,17 +1,43 @@
 #include <stdio.h>
+#include <stdint.h>
+#include "../Challenge3/main.c"
 
 int main(int argc, char* argv[]){
     if(argc < 2){
         printf("Must pass 1 argument\n");
     }
-    FILE *file = fopen("./input.txt","r");
-    if(file == NULL){
+    FILE *infile = fopen("./input.txt","r");
+    if(infile == NULL){
         perror("error when opening file");
         exit(1);
     }
-    int i=0;
-    char buffer[256];
-    while(fgets(buffer, sizeof(buffer), file)){
-        
+    FILE *outfile = fopen("./out.txt","w");
+    if(infile == NULL){
+        perror("error when opening file");
+        exit(1);
     }
+    char hexStringToBreak[256];
+    int i=0;
+    while(fgets(hexStringToBreak, sizeof(hexStringToBreak), infile)){
+        hexStringToBreak[strcspn(hexStringToBreak, "\n")] = '\0';
+        int rawStringToBreakLength = strlen(hexStringToBreak)/2;
+        
+        
+        int xorByte = singleByteXOR_getMostProbableRawByte(hexStringToBreak);
+        
+        uint8_t* rawStringToBreak = calloc(rawStringToBreakLength+1, sizeof(uint8_t));
+        hexStringToRawString(hexStringToBreak, rawStringToBreak);
+        uint8_t* singleByteRawString = createSingleByteRawString(xorByte, rawStringToBreakLength);
+        uint8_t* rawResultString = calloc((rawStringToBreakLength)+1, sizeof(uint8_t));
+
+        xorAgainstEncodedString_rawStrings(rawStringToBreak,singleByteRawString, rawResultString, rawStringToBreakLength);
+
+        fprintf(outfile, "i: %i || ", i);
+        fprintf(outfile, "xorByte: %x || ", xorByte);
+        fprintf(outfile, "string: %s\n",rawResultString);
+        i++;
+    }
+    fclose(infile);
+    fclose(outfile);
+    return 0;
 }
